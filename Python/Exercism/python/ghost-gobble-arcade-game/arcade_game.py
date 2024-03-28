ecb88991195f598ad376720fd94046527597b047
Joshua Yu <jyu6@cnm.edu>
2024-03-27 18:45:06 -0600

    clean up code according to suggestions from Exorcism

--- Python/Exercism/python/ghost-gobble-arcade-game/arcade_game.py
+++ Python/Exercism/python/ghost-gobble-arcade-game/arcade_game.py
@@ -15,14 +15,12 @@ def score(touching_power_pellet, touching_dot):
         point = False
     return point
 
-
 def lose(power_pellet_active, touching_ghost):
-    if power_pellet_active != True:
+    if power_pellet_active == False and touching_ghost == True:
         lost = True
     else:
         lost = False
     return lost
-
 
 def win(has_eaten_all_dots, power_pellet_active, touching_ghost):
     if has_eaten_all_dots == True and not (power_pellet_active == False and touching_ghost == True):
