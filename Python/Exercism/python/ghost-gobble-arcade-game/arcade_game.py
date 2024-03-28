aa56dcea286ca043f4f3b9606963d9f6cfcb94cb
Joshua Yu <jyu6@cnm.edu>
2024-03-27 18:35:46 -0600

    clean up code

--- Python/Exercism/python/ghost-gobble-arcade-game/arcade_game.py
+++ Python/Exercism/python/ghost-gobble-arcade-game/arcade_game.py
@@ -2,25 +2,26 @@
 
 
 def eat_ghost(power_pellet_active, touching_ghost):
-    if power_pellet_active and touching_ghost:
+    if power_pellet_active == True and touching_ghost == True:
         eaten = True
     else:
         eaten = False
     return eaten
 
 def score(touching_power_pellet, touching_dot):
-if touching_power_pellet or touching_dot:
+    if touching_power_pellet == True or touching_dot == True:
         point = True
     else:
         point = False
     return point
 
+
 def lose(power_pellet_active, touching_ghost):
-   if power_pellet_active != True:
-           lost = True
-       else:
-           lost = False
-       return lost
+    if power_pellet_active != True:
+        lost = True
+    else:
+        lost = False
+    return lost
 
 
 def win(has_eaten_all_dots, power_pellet_active, touching_ghost):
